5efa841b9c23aae0039e7fdb71998498375039a2
Kucing Elit <nekoicecream@outlook.co.id>
2021-11-27 13:49:43 +0700

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,7 +1,4 @@
-cp -R /var/cache/pacman/pkg/* /app
-cd /app
-repo-add nekoicecream.db.tar.gz *
-rsync -avrt rsync://ftp.byfly.by/pub/archlinux/core/os/any/ /app
+\rsync -avrt rsync://ftp.byfly.by/pub/archlinux/core/os/any/ /app
 rsync -avrt rsync://ftp.byfly.by/pub/archlinux/core/os/x86_64 /app
 rsync -avrt rsync://ftp.byfly.by/pub/archlinux/extra/os/any /app
 rsync -avrt rsync://ftp.byfly.by/pub/archlinux/extra/os/x86_64/ /app
