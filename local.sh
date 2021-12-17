07968b85c8a0e26b283e7bd16981b804e6c7155d
Kucing Elit <nekoicecream@outlook.co.id>
2021-12-17 16:00:47 +0700

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget https://www.linuxfromscratch.org/lfs/downloads/stable/wget-list
-wget --no-check-certificate --input-file=wget-list --continue --directory-prefix=/app
+wget --no-check-certificate --input-file=wget-list --continue 
